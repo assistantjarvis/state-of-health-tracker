604cb469a917c811137fd1e0b9836e9a3d2f5d7f
Chetan Mishra <138036874+assistantjarvis@users.noreply.github.com>
2025-07-17 01:54:17 +0530

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+  moduleNameMapper: {
+    '^@screens/(.*)$': '<rootDir>/src/screens/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
+    '^@theme/(.*)$': '<rootDir>/src/styles/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@data/(.*)$': '<rootDir>/src/data/$1', // This alias is for mock data
+    '^@service/(.*)$': '<rootDir>/src/service/$1', // This alias is for API services
+  },
